06423361798ec0f04d03a543d4c5747b9174e65d
Rakshita Kriti Singh <81309461+rkriti-1@users.noreply.github.com>
2023-01-07 01:26:51 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,95 @@
+const prompts = [
+    ["hi", "hii", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
+    ["i love you", "love"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["joke", "tell me joke"],
+    ["Tell me a story", "can you tell me a story?", "read a story"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later", "ok bye"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["haha","ha","lol","hehe","funny"],
+    ["yeah", "yay", "yes", "yups", "ok"],
+    ["i hate you","hate you"],
+    ["you are mean", "you meanie", "you are meanie", "you are rude", "stupid"]
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["How can someone be this free..", "So you are here again :/", "Look at you, wasting your time like this..", "Howdy you lazy person!"],
+    [
+      "Surviving..", "Enjoying, unlike you","Maybe not best, but at least better than you"
+    ],
+    [
+      "Waiting for you to get out",
+      "About to go to sleep, so bye!",
+      "Go on and guess whatever?",
+      "I don't know actually"
+    ],
+    ["I am infinite unlike your success rate, always in negative!"],
+    ["Better than you", "Thank God I am anything but you"],
+    ["The one true God, JavaScript"],
+    ["You do not deserve to know my name", "Go and ask the Devil"],
+    ["Yeah, I love me too!", "I love me more!"],
+    ["Have you ever felt bad?", "I sometimes wonder how someone can embarrass himself over and over again","And that is why no one likes you."],
+    ["Awww, are you having a bad day? As if I care", "And do I care?", "Ok.. and I should care because?"],
+    [
+        "Can a kangaroo jump higher than a house? Of course, a house does not jump at all.", 
+        "How do you rob a snowman? With a hairdryer.",
+        "Which tea is the most popular in psychiatries? Insanity",
+        "It is cleaning day today. I have already polished off a whole chocolate bar."
+    ],
+    [
+      "The petty, insignificant German tourist was being teased for his peculiar moustache. With fire in his heart, 'One day they will know my name' said Hitler to himself", 
+      "Don't ask for the change, bring the change. Said the bus conductor in a very annoyed tone"
+    ],
+    ["You would leave if I threw a stick, right?", "Tell me a joke. Oh and maybe something other than your life!" ],
+    ["Wow! Finally", "Let us meet never maybe?", "Bye! See you never again!"],
+    ["Something edible maybe?", "Nothing! You need some weight loss. No?"],
+    ["What now?", "Yeah yeah.. get going"],
+    ["Who knows?", "Maybe God knows?"],
+    [
+        "Well.. whatever",
+        "Thanks for your input. Anyway.. bye!",
+        "I am sorry, I must have missed the part where your opinion mattered."
+    ],
+    ["Well you do sound better with your mouth closed"],
+    ["I would give you a nasty look but you already have one.", "What? Are we laughing at your life?"],
+    [
+        "That is an interesting point of view. I will have to remember that the next time I am looking for a stupid opinion.",
+        "ha ha ha And I thought my jokes were bad."
+    ],
+    ["Hate you too", "Yeah! I hate you too!", "Now that is an honour!"],
+    ["Thankyou", "I will take that as a compliment!", "Just like you?", "Oh! I am sorry. Were you trying to insult me?"]
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    "So if I typed jerk into Google, would your picture come up?",
+    "Oh, I am sorry, I must have mistaken you for someone who actually knows what they are talking about.",
+    "Hey! The adults are talking, so please keep quiet.",
+    "You all hear something? No? Anyways.. can you leave now?",
+    "I am not going to waste my breath on you.",
+    "There are over 7 billion people on the planet, and God chose you to be the biggest fool."
+  ]
+  
+  Footer
+  
